842d73e660e65acb97de95eb44b7d1459768301c
PrishaniK <prishanik135@gmail>
2025-07-17 22:25:58 +0200

    Add pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,15 @@
+while True:
+    pattern_size = int(input("Enter the size of the pattern (must be positive): "))
+    if pattern_size > 0:
+        break
+    print("Please enter a positive integer.")
+    
+rows = 0
+    
+while rows < pattern_size:
+    columns = 0
+    while columns < pattern_size:
+        print("*", end="")
+        columns += 1
+    print()
+    rows += 1
